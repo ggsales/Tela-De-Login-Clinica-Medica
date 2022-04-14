b2fab6cbcfe6a9c58bf18d00d70657db3e3ebe06
ggsales <giovanisales14@gmail.com>
2022-04-14 15:11:54 -0300

    apply overlay in all content

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -7,9 +7,13 @@ export default function Layout() {
     return (
 
         <>
-            <video id='videoBG' autoPlay muted loop >
-                <source src='assets/videos/bg.mp4' type='video/mp4' />
-            </video>
+            <div className="video-box">
+                <video id='videoBG' autoPlay muted loop >
+                    <source src='assets/videos/bg.mp4' type='video/mp4' />
+                </video>
+                <div className="overlay-bg"></div>
+            </div>
+
         </>
     )
 }
